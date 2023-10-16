fa8cbec185320b2ec50173bb5b3bad79a8f0c0a4
666BLAD <briidacbc@gmail.com>
2023-10-16 12:14:14 +0100

    program média

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// fiz em C# porque já não tenho o dev cpp no computador e no visual studio só tenho C#.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double nota1, nota2, nota3, nota4, media;
+
+        Console.WriteLine("Insira primeira nota: ");
+        nota1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Insira segunda nota: ");
+        nota2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Insira terceira nota: ");
+        nota3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Insira quarta nota: ");
+        nota4 = Convert.ToDouble(Console.ReadLine());
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        Console.WriteLine("As notas são: ");
+        Console.WriteLine(nota1);
+        Console.WriteLine(nota2);
+        Console.WriteLine(nota3);
+        Console.WriteLine(nota4);
+
+        Console.WriteLine("A média é: ");
+        Console.WriteLine(media);
+
+        Console.ReadKey();
+    }
+}
